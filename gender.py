313ad62a4e438f0e07990c6c9a5195bc7f4f1713
seataK <297.1951@gmail.com>
2016-03-14 18:08:12 -0700

    Init

---
+++ gender.py
@@ -0,0 +1,268 @@
+import time
+
+import numpy as np
+import tensorflow as tf
+import tensorflow.models.rnn
+import reader
+
+flags = tf.flags
+logging = tf.logging
+
+flags.DEFINE_string("data_path", None, "data_path")
+flags.DEFINE_string(
+    "model", "small",
+    "A type of model. Possible options are: small, medium, large.")
+
+FLAGS = flags.FLAGS
+
+
+class GenderModel(object):
+    """Gender Classification model."""
+
+    def __init__(self, is_training, config):
+        self.batch_size = batch_size = config.batch_size
+        self.num_steps = num_steps = config.num_steps
+        size = config.hidden_size
+        vocab_size = config.vocab_size
+        num_classes = config.n_classes
+
+        self._input_data = tf.placeholder(tf.int32, [batch_size, num_steps])
+        self._targets = tf.placeholder(tf.float32, [batch_size, num_classes])
+
+        # Slightly better results can be obtained with forget gate biases
+        # initialized to 1 but the hyperparameters of the model would need to be
+        # different than reported in the paper.
+        lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(size, forget_bias=0.0)
+        if is_training and config.keep_prob < 1:
+            lstm_cell = tf.nn.rnn_cell.DropoutWrapper(
+                    lstm_cell, output_keep_prob=config.keep_prob)
+        cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * config.num_layers)
+
+        self._initial_state = cell.zero_state(batch_size, tf.float32)
+
+        with tf.device("/cpu:0"):
+            embedding = tf.get_variable("embedding", [vocab_size, size])
+            inputs = tf.nn.embedding_lookup(embedding, self._input_data)
+
+        if is_training and config.keep_prob < 1:
+            inputs = tf.nn.dropout(inputs, config.keep_prob)
+
+        inputs = [tf.squeeze(input_, [1])
+                  for input_ in tf.split(1, num_steps, inputs)]
+        outputs, state = tf.models.rnn.rnn.rnn(cell, inputs, initial_state=self._initial_state)
+
+        # hidden size == input size
+        # len_inputs == num_steps == num_outputs
+
+        print('vocab size', vocab_size)
+        print('size', size)
+        print('num_steps', num_steps)
+        print('len inputs', len(inputs))
+        print('inputs[0]', inputs[0])
+        print('batch_size', batch_size)
+        print('hidden states', size)
+        print('len_outputs', len(outputs))
+        print('output[0]', outputs[0])
+
+        print(tf.pack(outputs))
+        pooled = tf.reduce_mean(tf.pack(outputs), 0, name='pooling')
+        print(pooled)
+
+        softmax_w = tf.get_variable("softmax_w", [size, num_classes])
+        softmax_b = tf.get_variable("softmax_b", [num_classes])
+        logits = tf.matmul(pooled, softmax_w) + softmax_b
+        loss = tf.nn.softmax_cross_entropy_with_logits(logits, self._targets)
+        self._cost = cost = tf.reduce_mean(loss)
+        self._final_state = state
+
+        if not is_training:
+            return
+
+        self._lr = tf.Variable(0.0, trainable=False)
+        tvars = tf.trainable_variables()
+        grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars),
+                                          config.max_grad_norm)
+        optimizer = tf.train.GradientDescentOptimizer(self.lr)
+        self._train_op = optimizer.apply_gradients(zip(grads, tvars))
+
+    def assign_lr(self, session, lr_value):
+        session.run(tf.assign(self.lr, lr_value))
+
+    @property
+    def input_data(self):
+        return self._input_data
+
+    @property
+    def targets(self):
+        return self._targets
+
+    @property
+    def initial_state(self):
+        return self._initial_state
+
+    @property
+    def cost(self):
+        return self._cost
+
+    @property
+    def final_state(self):
+        return self._final_state
+
+    @property
+    def lr(self):
+        return self._lr
+
+    @property
+    def train_op(self):
+        return self._train_op
+
+
+class SmallConfig(object):
+    """Small config."""
+    init_scale = 0.1
+    learning_rate = 1.0
+    max_grad_norm = 5
+    num_layers = 2
+    num_steps = 20
+    hidden_size = 200
+    max_epoch = 4
+    max_max_epoch = 13
+    keep_prob = 1.0
+    lr_decay = 0.5
+    batch_size = 11 #20
+    # vocab_size = 10000
+    vocab_size = 89972
+    n_classes = 2
+
+
+class MediumConfig(object):
+    """Medium config."""
+    init_scale = 0.05
+    learning_rate = 1.0
+    max_grad_norm = 5
+    num_layers = 1
+    num_steps = 35
+    hidden_size = 650
+    max_epoch = 6
+    max_max_epoch = 39
+    keep_prob = 0.5
+    lr_decay = 0.8
+    batch_size = 20
+    vocab_size = 10000
+
+
+class LargeConfig(object):
+    """Large config."""
+    init_scale = 0.04
+    learning_rate = 1.0
+    max_grad_norm = 10
+    num_layers = 2
+    num_steps = 35
+    hidden_size = 1500
+    max_epoch = 14
+    max_max_epoch = 55
+    keep_prob = 0.35
+    lr_decay = 1 / 1.15
+    batch_size = 20
+    vocab_size = 10000
+
+
+class TestConfig(object):
+    """Tiny config, for testing."""
+    init_scale = 0.1
+    learning_rate = 1.0
+    max_grad_norm = 1
+    num_layers = 1
+    num_steps = 2
+    hidden_size = 2
+    max_epoch = 1
+    max_max_epoch = 1
+    keep_prob = 1.0
+    lr_decay = 0.5
+    batch_size = 20
+    vocab_size = 10000
+
+
+def run_epoch(session, m, data, eval_op, verbose=False):
+    """Runs the model on the given data."""
+    epoch_size = ((len(data) // m.batch_size) - 1) // m.num_steps
+    start_time = time.time()
+    costs = 0.0
+    iters = 0
+    state = m.initial_state.eval()
+    for step, (texts, label) in enumerate(reader.data_iterator(data, m.batch_size,
+                                                      m.num_steps)):
+        print('texts', texts.shape, texts)
+        print('labels', label.shape, label)
+
+        cost, state, _ = session.run([m.cost, m.final_state, eval_op],
+                                                                 {m.input_data: texts,
+                                                                    m.targets: label,
+                                                                    m.initial_state: state})
+        costs += cost
+        iters += m.num_steps
+
+        if verbose and step % (epoch_size // 10) == 10:
+            print("%.3f perplexity: %.3f speed: %.0f wps" %
+                        (step * 1.0 / epoch_size, np.exp(costs / iters),
+                         iters * m.batch_size / (time.time() - start_time)))
+
+    return np.exp(costs / iters)
+
+
+def get_config():
+    if FLAGS.model == "small":
+        return SmallConfig()
+    elif FLAGS.model == "medium":
+        return MediumConfig()
+    elif FLAGS.model == "large":
+        return LargeConfig()
+    elif FLAGS.model == "test":
+        return TestConfig()
+    else:
+        raise ValueError("Invalid model: %s", FLAGS.model)
+
+
+def main(_):
+    if not FLAGS.data_path:
+        raise ValueError("Must set --data_path to data directory")
+
+    raw_data = reader.converted_data(FLAGS.data_path, max_len=6960)
+
+    train_data, valid_data, test_data = reader.split_rawdata(raw_data)
+
+    print('td', train_data[0].shape)
+
+    config = get_config()
+    eval_config = get_config()
+    eval_config.batch_size = 1
+    eval_config.num_steps = 1
+
+    with tf.Graph().as_default(), tf.Session() as session:
+        initializer = tf.random_uniform_initializer(-config.init_scale,
+                                                    config.init_scale)
+        with tf.variable_scope("model", reuse=None, initializer=initializer):
+            m = GenderModel(is_training=True, config=config)
+        with tf.variable_scope("model", reuse=True, initializer=initializer):
+            mvalid = GenderModel(is_training=False, config=config)
+            mtest = GenderModel(is_training=False, config=eval_config)
+
+        tf.initialize_all_variables().run()
+
+        for i in range(config.max_max_epoch):
+            lr_decay = config.lr_decay ** max(i - config.max_epoch, 0.0)
+            m.assign_lr(session, config.learning_rate * lr_decay)
+
+            print("Epoch: %d Learning rate: %.3f" % (i + 1, session.run(m.lr)))
+            train_perplexity = run_epoch(session, m, train_data, m.train_op,
+                                                                     verbose=True)
+            print("Epoch: %d Train Perplexity: %.3f" % (i + 1, train_perplexity))
+            valid_perplexity = run_epoch(session, mvalid, valid_data, tf.no_op())
+            print("Epoch: %d Valid Perplexity: %.3f" % (i + 1, valid_perplexity))
+
+        test_perplexity = run_epoch(session, mtest, test_data, tf.no_op())
+        print("Test Perplexity: %.3f" % test_perplexity)
+
+
+if __name__ == "__main__":
+    tf.app.run()
---
+++ reader.py
@@ -0,0 +1,132 @@
+import numpy as np
+import tensorflow as tf
+
+import csv
+import os
+import re
+import collections
+import itertools
+from sklearn import preprocessing
+
+
+def read_csv(filepath):
+    with tf.gfile.GFile(filepath, "r") as f:
+        reader = csv.reader(f, delimiter=',')
+        return list(reader)
+
+def string_processing(str):
+    str = str.replace("\n", " ")
+    str = str.strip()
+    str = str.replace(".", " <EOS>")
+    str = str.replace("(", "( ")
+    str = str.replace(")", " )")
+    str = str.replace(",", " ,")
+    str = re.sub(r"\s+", " ", str)
+    return str
+
+
+def build_vocab(posts):
+    flattened = [word for post in posts for word in post]
+    counter = collections.Counter(flattened)
+
+    count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+
+    words, _ = list(zip(*count_pairs))
+    word_to_id = dict(zip(words, range(len(words))))
+
+    print('vocab_size:', len(words))
+
+    return word_to_id
+
+def converted_data(data_dir, min_nwords=200, max_len=None):
+    filepath = os.path.join(data_dir, "blog-gender-dataset.csv")
+
+    posts = []
+    labels = []
+
+    raw_data = read_csv(filepath)
+
+    for row in raw_data:
+        words = string_processing(row[0]).split(" ")
+        if len(words) < min_nwords:
+            continue
+
+        posts.append(words)
+        gen2num = {'f':0, 'm': 1}
+        labels.append(gen2num[row[1].strip().lower()])
+
+    lengths = [len(post) for post in posts]
+
+    if max_len is None:
+        max_len = max(lengths)
+
+    padded_posts = [[post[j] if j < lengths[i] else "<PAD>" for j in range(max_len)]
+                                                            for i, post in enumerate(posts)]
+
+    word_to_id = build_vocab(padded_posts)
+
+    id_posts = np.array([[word_to_id[word] for word in post] for post in padded_posts], dtype=np.int32)
+    labels = np.array(labels)
+
+    bin_labels = np.zeros((labels.shape[0], 2))
+    bin_labels[labels==0, 0] = 1
+    bin_labels[labels==1, 1] = 1
+
+    return id_posts, bin_labels
+
+
+def split_rawdata(raw_data, ratio=[.7, .1, .2]):
+    posts, labels = raw_data
+    n_data = len(labels)
+
+    n_train = int(n_data * ratio[0])
+    n_valid = int(n_data * ratio[1])
+
+    return (posts[:n_train], labels[:n_train]),\
+           (posts[n_train:n_train+n_valid], labels[n_train:n_train+n_valid]),\
+           (posts[n_train+n_valid:], labels[n_train+n_valid:])
+
+
+def print_stats(posts, labels):
+    len_posts = [len(p) for p in posts]
+
+    print('num posts:', len(posts))
+    print('ave words:', sum(len_posts) / len(posts))
+    print('min words:', min(len_posts))
+    print('max words:', max(len_posts))
+
+    print('num Male:', sum(labels))
+    print('num Female:', len(posts) - sum(labels))
+
+
+def data_iterator(data, batch_size, num_steps):
+    """
+    :param data:
+    :param batch_size: num of posts in a batch
+    :param num_steps: num of words in a single row of batch
+    :return:
+    """
+    posts, labels = data
+
+    num_posts, len_post = posts.shape
+    assert num_posts == labels.shape[0]
+
+    len_batch = num_posts // batch_size
+    len_step = len_post // num_steps
+
+    if len_batch < 1:
+        raise ValueError("batch_size must be <= num posts")
+
+    if len_step < 1:
+        raise ValueError("num_step must be <= len_posts")
+
+    for i_batch in range(len_batch):
+        for i_step in range(len_step):
+            yield posts[i_batch*batch_size:(i_batch+1)*batch_size, i_step*num_steps:(i_step+1)*num_steps],\
+                  labels[i_batch*batch_size:(i_batch+1)*batch_size]
+
+
+if __name__ == '__main__':
+    print('reader main')
+    posts, labels = converted_data('data', min_nwords=100)
+    print_stats(posts, labels)
